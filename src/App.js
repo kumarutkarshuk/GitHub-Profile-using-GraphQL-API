57b36103eb593c33825fd090526ebf78ffa601a9
kumarutkarshuk <kumarutkarshuk18@gmail.com>
2023-10-13 23:15:28 +0530

    Front-end updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
-import "./App.css";
+
+import { useState } from "react";
+import Data from "./components/Data";
+import { MdDarkMode, MdLightMode } from "react-icons/md";
+import {AiOutlineSearch} from "react-icons/ai";
+import '@fontsource/space-mono';
 
 function App() {
+
+  const [isLight, setIsLight] = useState(true); //have to store preference and use the function for updating
+
   return (
-    <div>App</div>
+    <div className="h-screen w-screen flex justify-center items-center bg-[#141D2F] text-white font-mono"> 
+    {/* text white */}
+      <div className="flex flex-col gap-[36px]">
+
+        <div className="flex">
+          <h1>GitHub</h1>
+
+          <div className="flex items-center w-[90px] tracking-[2.5px]
+                          font-bold">
+            <p>
+              {isLight ? "LIGHT" : "DARK"}
+            </p>
+            {isLight ? <MdLightMode/> : <MdDarkMode/>}
+          </div>
+        </div>
+        
+        <div className="bg-[#1E2A47] relative rounded-[15px] flex h-[100%] justify-center items-center">
+        {/* box shadow and background for icon*/}
+          <input type="text" placeholder="Enter a GitHub username..."/>
+          <AiOutlineSearch/>
+          {/* icon styling */}
+          <button className="bg-[#0079ff] hover:bg-[#60abff] font-bold 
+          min-w-[84px] rounded-[10px]">Search</button>
+        </div>
+
+        <div>
+          <Data></Data>
+        </div>
+
+        </div>
+
+    </div>
   );
 }
 
