405927c08a858e44d7ebce45b9c4485633cd33f6
kumarutkarshuk <kumarutkarshuk18@gmail.com>
2023-10-14 12:05:03 +0530

    Front-end updated

--- src/App.js
+++ src/App.js
@@ -10,29 +10,29 @@ function App() {
   const [isLight, setIsLight] = useState(true); //have to store preference and use the function for updating
 
   return (
-    <div className="h-screen w-screen flex justify-center items-center bg-[#141D2F] text-white font-mono"> 
-    {/* text white */}
-      <div className="flex flex-col gap-[36px]">
+    <div className="h-screen w-screen flex justify-center bg-[#141D2F] text-white font-mono text-xl"> 
+    
+      <div className="flex flex-col gap-5 w-[35%] my-5">
 
-        <div className="flex">
-          <h1>GitHub</h1>
+        <div className="flex justify-between">
+          <h1 className="font-extrabold">DevDetective</h1>
 
-          <div className="flex items-center w-[90px] tracking-[2.5px]
-                          font-bold">
+          <div className="flex items-center w-[15%] 
+                           justify-center text-base gap-1">
             <p>
               {isLight ? "LIGHT" : "DARK"}
             </p>
             {isLight ? <MdLightMode/> : <MdDarkMode/>}
           </div>
+
         </div>
         
-        <div className="bg-[#1E2A47] relative rounded-[15px] flex h-[100%] justify-center items-center">
-        {/* box shadow and background for icon*/}
-          <input type="text" placeholder="Enter a GitHub username..."/>
+        <div className="bg-[#1E2A47] flex justify-evenly items-center rounded-lg w-full h-12 p-1 gap-2">
           <AiOutlineSearch/>
-          {/* icon styling */}
+          <input type="text" placeholder="Enter a GitHub username..." className=" bg-inherit w-[75%] focus:outline-none"/>
+          
           <button className="bg-[#0079ff] hover:bg-[#60abff] font-bold 
-          min-w-[84px] rounded-[10px]">Search</button>
+                              rounded-md px-3 py-2 text-base">Search</button>
         </div>
 
         <div>
