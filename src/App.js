46d5db34b8ed76454d82e48f223586903283fac1
kumarutkarshuk <kumarutkarshuk18@gmail.com>
2023-10-15 22:23:25 +0530

    Made responsive

--- src/App.js
+++ src/App.js
@@ -29,22 +29,23 @@ function App() {
   return (
     <div className="min-h-screen w-screen flex justify-center bg-[#141D2F] text-white font-mono text-xl"> 
     
-      <div className="flex flex-col gap-6 w-[35%] my-5 items-center">
+      <div className="flex flex-col gap-6 w-[80%] md:w-[35%] my-5 items-center">
         
-        <div className="flex items-center gap-2">
+        <div className="flex flex-col justify-center items-center text-center md:flex-row md:items-center gap-2">
           <AiFillGithub/>
           <h1 className="font-extrabold">Search GitHub Profile</h1>
         </div>
         
         
-        <div className="bg-[#1E2A47] flex justify-evenly items-center rounded-xl w-full h-14 p-1 gap-2">
-          <AiOutlineSearch/>
-          <input type="text" placeholder="Enter a GitHub username..." className=" bg-inherit w-[75%] focus:outline-none" onChange={changeHandler}
+        <div className="bg-[#1E2A47] flex justify-evenly items-center rounded-xl w-full h-14 md:p-1 gap-2">
+          <div className="hidden md:block"><AiOutlineSearch/></div>
+          <input type="text" placeholder="Enter a GitHub username..." className=" bg-inherit w-[75%] focus:outline-none text-base h-[100%] 
+          md:text-lg" onChange={changeHandler}
             onKeyDown={keyHandler}
           />
-          
           <button className="bg-[#0079ff] hover:bg-[#60abff] font-bold 
-                              rounded-md px-3 py-2 text-base" onClick={clickHandler} >Search</button>
+                              rounded-md px-3 py-2 text-base hidden md:block" onClick={clickHandler} >Search</button>
+          <button className="block md:hidden" onClick={clickHandler}><AiOutlineSearch/></button>
         </div>
 
         
